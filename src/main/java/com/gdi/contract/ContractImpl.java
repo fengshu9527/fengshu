a3c5f3c1f80a3f6bb3b245d062ae794c5135b6e0
fengshu9527 <32172354+fengshu9527@users.noreply.github.com>
2017-09-29 09:56:00 +0800

    Update ContractImpl.java

--- src/main/java/com/gdi/contract/ContractImpl.java
+++ src/main/java/com/gdi/contract/ContractImpl.java
@@ -6,7 +6,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
-import org.web3j.abi.FunctionEncoder;
 import org.web3j.abi.TypeReference;
 import org.web3j.abi.datatypes.Address;
 import org.web3j.abi.datatypes.Bool;
@@ -19,11 +18,7 @@ import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.tx.Contract;
-import org.web3j.utils.Async;
-import org.web3j.utils.Numeric;
-
 import com.gdi.bean.pojo.Order;
-import com.gdi.util.Consts;
 import com.gdi.util.Util;
 
 /**
@@ -52,19 +47,19 @@ public class ContractImpl extends Contract implements ContractInterface {
 
 	@Override
 	public Future<TransactionReceipt> addUser(String userAddress, String password, String userName) {
-		Function function = new Function("addUser", Arrays.<Type>asList(new Address(userAddress),Util.stringToByte32(password),new Utf8String(userName)), Collections.<TypeReference<?>>emptyList());
+		Function function = new Function("addUser", Arrays.<Type>asList(new Address(userAddress),Util.stringToByte32(password),Util.stringToByte32(userName)), Collections.<TypeReference<?>>emptyList());
 		return executeTransactionAsync(function); 
 	}
 
 	@Override
 	public Future<Bool> hasAlreadyRegister(String userName) {
-		Function function = new Function("hasAlreadyRegister", Arrays.asList(new Utf8String(userName)),Arrays.<TypeReference<?>>asList(new TypeReference<Bool>() {}));
+		Function function = new Function("hasAlreadyRegister", Arrays.asList(Util.stringToByte32(userName)),Arrays.<TypeReference<?>>asList(new TypeReference<Bool>() {}));
 		return executeCallSingleValueReturnAsync(function);
 	}
 
 	@Override
 	public CompletableFuture<List<Type>> getPasswordByUserName(String userName) {
-		Function function = new Function("userPassByName", Arrays.asList(new Utf8String(userName)), 
+		Function function = new Function("userPassByName", Arrays.asList(Util.stringToByte32(userName)), 
 				Arrays.<TypeReference<?>>asList(new TypeReference<Bytes32>() {
 				}, new TypeReference<Address>() {
 				}));
@@ -76,10 +71,9 @@ public class ContractImpl extends Contract implements ContractInterface {
 		Function function = new Function("existIcoProject", Arrays.asList(Util.stringToByte32(icoName)),Arrays.<TypeReference<?>>asList(new TypeReference<Bool>() {}));
 		return executeCallSingleValueReturnAsync(function);
 	}
-
 	@Override
-	public Future<TransactionReceipt> addIcoProject(String icoName, String jsonvalue, int preAmount, Long startTime, Long endTime) {
-		Function function = new Function("addICOProject", Arrays.<Type>asList(Util.stringToByte32(icoName), new Utf8String(jsonvalue), new Uint256(preAmount), new Uint256(startTime), new Uint256(endTime)), Collections.<TypeReference<?>>emptyList());
+	public Future<TransactionReceipt> addIcoProject(String icoName, String jsonvalue, int preAmount, Long startTime, Long endTime, String userName) {
+		Function function = new Function("addICOProject", Arrays.<Type>asList(Util.stringToByte32(icoName), new Utf8String(jsonvalue), new Uint256(preAmount), new Uint256(startTime), new Uint256(endTime), Util.stringToByte32(userName)), Collections.<TypeReference<?>>emptyList());
 		return executeTransactionAsync(function); 
 	}
 
@@ -124,25 +118,25 @@ public class ContractImpl extends Contract implements ContractInterface {
 
 	@Override
 	public Future<TransactionReceipt> attentionICOProject(String icoName, String userName) {
-		Function function = new Function("attentionICOProject", Arrays.<Type>asList(Util.stringToByte32(icoName),new Utf8String(userName)), Collections.<TypeReference<?>>emptyList());
+		Function function = new Function("attentionICOProject", Arrays.<Type>asList(Util.stringToByte32(icoName),Util.stringToByte32(userName)), Collections.<TypeReference<?>>emptyList());
 		return executeTransactionAsync(function); 
 	}
 
 	@Override
 	public Future<TransactionReceipt> removeAttentionICOProject(String icoName, String userName) {
-		Function function = new Function("removeAttentionICOProject", Arrays.<Type>asList(Util.stringToByte32(icoName),new Utf8String(userName)), Collections.<TypeReference<?>>emptyList());
+		Function function = new Function("removeAttentionICOProject", Arrays.<Type>asList(Util.stringToByte32(icoName),Util.stringToByte32(userName)), Collections.<TypeReference<?>>emptyList());
 		return executeTransactionAsync(function); 
 	}
 
 	@Override
 	public Future<Bool> hasAttention(String icoName, String userName) {
-		Function function = new Function("hasAttention", Arrays.asList(Util.stringToByte32(icoName), new Utf8String(userName)),Arrays.<TypeReference<?>>asList(new TypeReference<Bool>() {}));
+		Function function = new Function("hasAttention", Arrays.asList(Util.stringToByte32(icoName), Util.stringToByte32(userName)),Arrays.<TypeReference<?>>asList(new TypeReference<Bool>() {}));
 		return executeCallSingleValueReturnAsync(function);
 	}
 
 	@Override
 	public Future<Uint256> myAttentionNumber(String userName) {
-		Function function = new Function("myAttentionNumber", Arrays.asList(new Utf8String(userName)),
+		Function function = new Function("myAttentionNumber", Arrays.asList(Util.stringToByte32(userName)),
 				Arrays.<TypeReference<?>>asList(new TypeReference<Uint256>() {
 				}));
 		return executeCallSingleValueReturnAsync(function);
@@ -150,7 +144,7 @@ public class ContractImpl extends Contract implements ContractInterface {
 	
 	@Override
 	public Future<Uint256> allAttentionNumber(String userName) {
-		Function function = new Function("allAttentionNumber", Arrays.asList(new Utf8String(userName)),
+		Function function = new Function("allAttentionNumber", Arrays.asList(Util.stringToByte32(userName)),
 				Arrays.<TypeReference<?>>asList(new TypeReference<Uint256>() {
 				}));
 		return executeCallSingleValueReturnAsync(function);
@@ -158,7 +152,7 @@ public class ContractImpl extends Contract implements ContractInterface {
 
 	@Override
 	public CompletableFuture<List<Type>> myAttentionIcoInfo(String userName, int i) {
-		Function function = new Function("myAttentionIcoInfo", Arrays.asList(new Utf8String(userName),new Uint256(BigInteger.valueOf(i))),
+		Function function = new Function("myAttentionIcoInfo", Arrays.asList(Util.stringToByte32(userName),new Uint256(BigInteger.valueOf(i))),
 				Arrays.<TypeReference<?>>asList(new TypeReference<Bool>() {
 				}, new TypeReference<Utf8String>() {
 				}, new TypeReference<Uint256>() {
@@ -182,7 +176,7 @@ public class ContractImpl extends Contract implements ContractInterface {
 	@Override
 	public CompletableFuture<List<Type>> fetchInvetstor(String icoName, int i) {
 		Function function = new Function("fetchInvetstor", Arrays.asList(Util.stringToByte32(icoName),new Uint256(BigInteger.valueOf(i))),
-				Arrays.<TypeReference<?>>asList(new TypeReference<Utf8String>() {
+				Arrays.<TypeReference<?>>asList(new TypeReference<Bytes32>() {
 				}, new TypeReference<Uint256>() {
 				}));
 		return executeCallMultipleValueReturnAsync(function);
@@ -198,7 +192,7 @@ public class ContractImpl extends Contract implements ContractInterface {
 
 	@Override
 	public Future<TransactionReceipt> storeUserInvest(Order order) {
-		Function function = new Function("storeUserInvest", Arrays.<Type>asList(new Address(order.getUserAddress()),Util.stringToByte32(order.getTokenName()), new Utf8String(order.getTradeAddr()), new Uint256(BigInteger.valueOf(order.getAmount())),new Utf8String(order.getDate())), Collections.<TypeReference<?>>emptyList());
+		Function function = new Function("storeUserInvest", Arrays.<Type>asList(new Address(order.getUserAddress()),Util.stringToByte32(order.getTokenName()), new Utf8String(order.getTradeAddr()), new Uint256(BigInteger.valueOf(order.getAmount())),Util.stringToByte32(order.getDate())), Collections.<TypeReference<?>>emptyList());
 		return executeTransactionAsync(function); 
 	}
 
@@ -217,8 +211,74 @@ public class ContractImpl extends Contract implements ContractInterface {
 				}, new TypeReference<Bytes32>() {
 				}, new TypeReference<Utf8String>() {
 				}, new TypeReference<Uint256>() {
+				}, new TypeReference<Bytes32>() {
+				}));
+		return executeCallMultipleValueReturnAsync(function);
+	}
+	
+	@Override
+	public Future<Uint256> getMyPublishedProjectCount(String username) {
+		Function function = new Function("getMyPublishedProjectCount", Arrays.asList(Util.stringToByte32(username)),
+				Arrays.<TypeReference<?>>asList(new TypeReference<Uint256>() {
+				}));
+		return executeCallSingleValueReturnAsync(function);
+	}
+
+	@Override
+	public CompletableFuture<List<Type>> getMyPublishedProject(String username, int i) {
+		Function function = new Function("getMyPublishedProject", Arrays.asList(Util.stringToByte32(username),new Uint256(BigInteger.valueOf(i))),
+				Arrays.<TypeReference<?>>asList(new TypeReference<Utf8String>() {
+				}, new TypeReference<Uint256>() {
+				}, new TypeReference<Uint256>() {
+				}));
+		return executeCallMultipleValueReturnAsync(function);
+	}
+
+	@Override
+	public Future<Uint256> getMyInvestedProjectCount(String username) {
+		Function function = new Function("getMyInvestedProjectCount", Arrays.asList(Util.stringToByte32(username)),
+				Arrays.<TypeReference<?>>asList(new TypeReference<Uint256>() {
+				}));
+		return executeCallSingleValueReturnAsync(function);
+	}
+
+	@Override
+	public CompletableFuture<List<Type>> getMyInvestedProject(String username, int i) {
+		Function function = new Function("getMyInvestedProject", Arrays.asList(Util.stringToByte32(username),new Uint256(BigInteger.valueOf(i))),
+				Arrays.<TypeReference<?>>asList(new TypeReference<Utf8String>() {
+				}, new TypeReference<Uint256>() {
+				}, new TypeReference<Uint256>() {
+				}));
+		return executeCallMultipleValueReturnAsync(function);
+	}
+
+	@Override
+	public Future<TransactionReceipt> addAssessor(String personAddr, String password, String phone, String validDateInfo, String jsonvalue) {
+		Function function = new Function("addAssessor", Arrays.<Type>asList(new Address(personAddr),Util.stringToByte32(password), Util.stringToByte32(phone), new Utf8String(validDateInfo), new Utf8String(jsonvalue)), Collections.<TypeReference<?>>emptyList());
+		return executeTransactionAsync(function);
+	}
+
+	@Override
+	public Future<Bool> hasPhoneRegistered(String phone) {
+		Function function = new Function("hasPhoneRegistered", Arrays.asList(Util.stringToByte32(phone)),Arrays.<TypeReference<?>>asList(new TypeReference<Bool>() {}));
+		return executeCallSingleValueReturnAsync(function);
+	}
+
+	@Override
+	public CompletableFuture<List<Type>> assessorPassByPhone(String phone) {
+		Function function = new Function("assessorPassByPhone", Arrays.asList(Util.stringToByte32(phone)), 
+				Arrays.<TypeReference<?>>asList(new TypeReference<Bytes32>() {
+				}, new TypeReference<Address>() {
+				}, new TypeReference<Bytes32>() {
+				}, new TypeReference<Utf8String>() {
 				}, new TypeReference<Utf8String>() {
 				}));
 		return executeCallMultipleValueReturnAsync(function);
+	}
+
+	@Override
+	public Future<TransactionReceipt> modifyValidDate(String phone, String validDateInfo) {
+		Function function = new Function("modifyValidDate", Arrays.<Type>asList(Util.stringToByte32(phone), new Utf8String(validDateInfo)), Collections.<TypeReference<?>>emptyList());
+		return executeTransactionAsync(function);
 	}
 }
